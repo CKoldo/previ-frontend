e6183e78f7a536acb22ce38179ec21a677efe967
LCorp <cohakoldo@gmail.com>
2026-03-11 00:40:30 -0500

    Actualización del frontend 2

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   //api for despliegue
-  //API_URL_BASE: 'https://ece1-2800-4b0-4002-3b26-600b-1162-d311-be0d.ngrok-free.app/',
-  API_URL_BASE: 'http://localhost:4000/',
+  API_URL_BASE: 'https://locking-contain-distances-vbulletin.trycloudflare.com/',
+  //API_URL_BASE: 'http://localhost:4000/',
   //API_URL_BASE: 'https://previ-backend.vercel.app/',
   MINEDU_DNI: '11223344'
 }; 
