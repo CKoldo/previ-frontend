e8105fb905e4e7d0e36419dbab25a11bb4b0c2b6
LCorp <cohakoldo@gmail.com>
2026-03-17 07:42:49 -0500

    cambios env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   //api for despliegue
-  API_URL_BASE: 'https://cameras-landing-scotia-strips.trycloudflare.com/',
+  API_URL_BASE: 'https://assessing-tires-covering-pure.trycloudflare.com/',
   //API_URL_BASE: 'http://localhost:4000/',
   //API_URL_BASE: 'https://previ-backend.vercel.app/',
   MINEDU_DNI: '11223344'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   //API_URL_BASE: 'https://previ-backend.vercel.app/',
-  API_URL_BASE: 'https://cameras-landing-scotia-strips.trycloudflare.com/',
+  API_URL_BASE: 'https://assessing-tires-covering-pure.trycloudflare.com/',
   MINEDU_DNI:'11223344'
 };
