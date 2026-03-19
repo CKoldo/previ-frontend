a41e98b684dca5607b07f04a27faa524b9b7dc47
LCorp <cohakoldo@gmail.com>
2026-03-19 09:48:54 -0500

    cambios en api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   //API_URL_BASE: 'https://previ-backend.vercel.app/',
-  API_URL_BASE: 'https://assessing-tires-covering-pure.trycloudflare.com/',
+  API_URL_BASE: 'https://divx-encourages-rca-denver.trycloudflare.com/',
   MINEDU_DNI:'11223344'
 };
