bd1f617c428583e552c8403c6fbf200aaf49657c
CKoldo <101054350+CKoldo@users.noreply.github.com>
2026-03-23 09:43:13 -0500

    Update API_URL_BASE for development environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   //api for despliegue
-  API_URL_BASE: 'https://strategy-statements-feels-senate.trycloudflare.com/',
+  API_URL_BASE: 'https://revealed-apparatus-len-ipaq.trycloudflare.com/',
   //API_URL_BASE: 'http://localhost:4500/',
   //API_URL_BASE: 'https://previ-backend.vercel.app/',
   MINEDU_DNI: '11223344'
