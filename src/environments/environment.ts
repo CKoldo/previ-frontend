07a745af6252ab0859344bca5bc1007938f2e578
LCorp <cohakoldo@gmail.com>
2026-03-25 11:47:14 -0500

    CAMBIOS API PARA PRUEBAS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //API_URL_BASE: 'https://previ-backend.vercel.app/',
-  API_URL_BASE: 'https://strategy-statements-feels-senate.trycloudflare.com/',
+  API_URL_BASE: 'https://load-dns-cult-dozens.trycloudflare.com/',
   //API_URL_BASE: 'http://localhost:4500/',
   MINEDU_DNI:'11223344'
 };
