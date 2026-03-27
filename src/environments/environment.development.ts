2989086fcc87db0665f5c1f0423d0a35ef52f43c
LCorp <cohakoldo@gmail.com>
2026-03-27 10:10:49 -0500

    cambios conexion api 27/03

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   //api for despliegue
-  API_URL_BASE: 'https://suited-liable-holder-councils.trycloudflare.com/',
+  API_URL_BASE: 'https://rotary-pills-isle-available.trycloudflare.com/',
   //API_URL_BASE: 'http://localhost:4500/',
   //API_URL_BASE: 'https://previ-backend.vercel.app/',
   MINEDU_DNI: '11223344'
