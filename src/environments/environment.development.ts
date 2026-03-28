52dc49b34cd21dded7a2ff88f58758af06259fc1
CKoldo <101054350+CKoldo@users.noreply.github.com>
2026-03-28 14:55:13 -0500

    Update API_URL_BASE for development environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API_URL_BASE: 'https://downtown-loud-discrete-device.trycloudflare.com/',
+  API_URL_BASE: 'https://segments-covering-princess-systematic.trycloudflare.com/',
   //API_URL_BASE: 'http://localhost:4000/',
   //API_URL_BASE: 'https://previ-backend.vercel.app/',
   MINEDU_DNI: '11223344'
