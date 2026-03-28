d1b267ed55fb17850212111a91fcacae163f4b96
CKoldo <101054350+CKoldo@users.noreply.github.com>
2026-03-28 18:35:11 -0500

    Uncomment API_URL_BASE for production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //API_URL_BASE: 'https://backend-previ.luedcorp.uk/
+  API_URL_BASE: 'https://backend-previ.luedcorp.uk/
   //API_URL_BASE: 'http://localhost:4000/',
   MINEDU_DNI:'11223344'
 };
